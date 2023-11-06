5be896f2142aa736aaa65d58687fad17d583dce1
Maria Fe Monares Sabar <mafs@marias-mbp.mynetworksettings.com>
2023-11-06 16:13:21 -0500

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+// create a string user: You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+
+//Assigning 3 variables 
+//sum of 
+let num1 = 8+2;
+let num2 = 2*20;
+let num3 = 117/3;
+//displaying combo
+alert(`The combination of the lock is ${num1} - ${num2} - ${num3}.`)
---
+++ practice.js
@@ -0,0 +1,19 @@
+console.log("hello world!");
+console.log(1 + 1)
+console.log(1 + "1")
+// we don't use va anymore
+var mafe1 = "mafe"
+
+//conts and let are the modern ways to writing variable
+
+//const cannot be reaassigned, the value will never change
+const mafe2 = "mafe"
+//mafe2=mafe sabar
+
+//let CAN be reassigned, the value is malleable
+let mafe3 = "mafe"
+mafe3 = "mafe sabar"
+
+console.log("mafe3",mafe3)
+console.log("My name is " +mafe3 + ".")
+console.log(`My name is ${mafe3}.`)
